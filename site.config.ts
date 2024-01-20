0ded0501951727c47709e18b7fa1ffd67318b48f
Cynthia <cynthia348348@gmail.com>
2024-01-20 14:12:29 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f32390abdce84b6694949c8826a9327e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Cynthia Portfolio',
+  domain: 'https://cynthiayip.notion.site/Cynthia-Yip-f32390abdce84b6694949c8826a9327e?pvs=4',
+  author: 'Cynthia Yip',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
